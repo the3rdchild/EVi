d38671bdc6035e1516b7e754ecb8a8101873cb58
The3rdChild <ytcrash555@gmail.com>
2024-11-06 10:26:40 +0700

    update

--- run.bat
+++ run.bat
@@ -1,11 +1,9 @@
 @echo off
-REM parent directory EVi
-set BASE_DIR=%~dp0EVi
 
-REM run main.py
-python "%BASE_DIR%\main.py"
+REM run main.py 
+start cmd /k "python .\main.py"
 
 REM run dbmeter.py
-python "%BASE_DIR%\dbmeter\dbmeter.py"
+start cmd /k "python .\dbmeter\dbmeter.py"
 
 pause
